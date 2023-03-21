06c8aeb82df426119caacc2f1f2534b529e9c3b3
ToUNVRSe <2956361755@qq.com>
2023-03-22 07:39:54 +0800

    优化 pandoc 适配

--- scripts/filters/pandoc.js
+++ scripts/filters/pandoc.js
@@ -2,12 +2,13 @@
 
 'use strict';
 
-if (hexo.config.pandoc) {
-  hexo.extend.filter.register('after_post_render', (data) => {
-    data.content = data.content.replaceAll(/(?<=<h[1-6] id=".*">).*(?=<\/h[1-6]>)/g, (match) => {
-      let link = match.replace(' ', '-')
+hexo.extend.filter.register('after_post_render', (data) => {
+  data.content = data.content.replaceAll(/(?<=<h[1-6] id=".*">).*(?=<\/h[1-6]>)/g, (match) => {
+    let link = match.replace(' ', '-')
+    if (!match.match(/<a href="#.*?" class="headerlink" title=".*?"><\/a>/g)) {
       return `<a href="#${link}" class="headerlink" title="${link}"></a>` + match
-    })
-    return data
-  });
-}
+    }
+    return match
+  })
+  return data
+});
